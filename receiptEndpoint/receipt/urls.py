60114f39ea0508ce04f03f719ad3d3e656c47e4c
mercuryedge117 <mercucyedge@gmail.com>
2023-10-20 15:38:13 -0500

    Revert "minor fix v1"
    This reverts commit 78d09031ccd9ba217c0f17a10791a7e37f4ef7c5.

--- receiptEndpoint/receipt/urls.py
+++ receiptEndpoint/receipt/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('process', views.ReceiptView.as_view()),
-    re_path(r'^(?P<id>\S{36})/points', views.ReceiptView.as_view())
+    path('receipts/process', views.ReceiptView.as_view()),
+    re_path(r'^receipts/(?P<id>\S{36})/points', views.ReceiptView.as_view())
 ]
