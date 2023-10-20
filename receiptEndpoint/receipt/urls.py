1b349c7fd6c6dd62f98edec872f55de0105a8eed
mercuryedge117 <mercucyedge@gmail.com>
2023-10-20 16:06:46 -0500

    minor fix 2, follow id schema

--- receiptEndpoint/receipt/urls.py
+++ receiptEndpoint/receipt/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('process', views.ReceiptView.as_view()),
-    re_path(r'^(?P<id>\S{36})/points', views.ReceiptView.as_view())
+    re_path(r'^(?P<id>\S+)/points', views.ReceiptView.as_view())
 ]
